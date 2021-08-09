9b9cd05de6e24706293bfc06a47a60677c2e42d4
Ahmad-A92-3 <ahmad.alsyad92@gmail.com>
2021-08-09 22:15:22 +0300

    code challenge 27-merge sort

--- challenges/sort/README.md
+++ challenges/sort/README.md
@@ -1,3 +1,9 @@
+### Code Challenge 26
 in this challenge we created a method that accepted an array and sorted its item from smaller to bigger 
 - [WhiteBoarde Link](https://miro.com/app/board/o9J_l9Wc_5c=/)
 - ![image](whiteBoard.PNG)
+
+### Code Challenge 27
+in this challenge we createdcreate sort merge; on which merge sort is one of the most efficient sorting algorithms. It works on the principle of Divide and Conquer. Merge sort repeatedly breaks down a list into several sublists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list.
+- [WhiteBoarde Link](https://miro.com/app/board/o9J_l9Wc_5c=/)
+- ![image](whiteBoard-CodeChallenge27.PNG)
--- challenges/sort/lib/src/main/java/sort/Library.java
+++ challenges/sort/lib/src/main/java/sort/Library.java
@@ -10,9 +10,11 @@ public class Library {
     public static void main(String[] args) {
 
         int[] array=  {8,4,23,42,16,15};
-        SelectionSort(array);
+//        SelectionSort(array);
+        mergeSort(array,0,5);
         System.out.println(array[0]);
         System.out.println(array[5]);
+
 
     }
 
@@ -30,6 +32,48 @@ public class Library {
             }
             arr[i]=value;
         }
+    }
+
+    private static void merge(int arr[], int start, int mid, int end ){
+        int [] temp= new int[end-start+1];
+        int i=start,j=mid+1,k=0;
+        while (i<=mid && j<=end){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                k+=1;
+                i+=1;
+            }else{
+                temp[k]=arr[j];
+                k+=1;
+                j+=1;
+            }
+        }
+
+        while(i<=mid){
+            temp[k]=arr[i];
+            i+=1;
+            k+=1;
+        }
+
+        while(j<=end){
+            temp[k]=arr[j];
+            j+=1;
+            k+=1;
+        }
+
+        for(i = start; i <= end; i += 1) {
+            arr[i] = temp[i - start];
+        }
+    }
+
+    public static void mergeSort(int [] arr, int start,int end){
+        if (start<end){
+            int mid= (end+start)/2;
+            mergeSort(arr,start,mid);
+            mergeSort(arr,mid+1,end);
+            merge(arr,start,mid,end);
+        }
+
     }
 
 
