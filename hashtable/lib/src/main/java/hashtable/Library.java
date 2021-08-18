5bfa480e6fa0daa5d8ef699d85f4be1a5b6b63e8
Ahmad-A92-3 <ahmad.alsyad92@gmail.com>
2021-08-18 08:38:35 +0300

    code challenge 33- Left Join

--- hashtable/lib/src/main/java/hashtable/Library.java
+++ hashtable/lib/src/main/java/hashtable/Library.java
@@ -9,6 +9,7 @@ import tree.BinaryTree;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class Library {
     public boolean someLibraryMethod() {
@@ -28,6 +29,25 @@ public class Library {
         tree2.Add(70);
         System.out.println(intersection(tree1,tree2));
 
+        // code challenge 33 leftJoin
+
+        HashMap<String,String> map1= new HashMap<>();
+        map1.put("fond","enammored");
+        map1.put("wrath","anger");
+        map1.put("diligent","employed");
+        map1.put("outift","garb");
+        map1.put("guide","usher");
+
+        HashMap<String,String> map2= new HashMap<>();
+        map2.put("fond","averse");
+        map2.put("wrath","delight");
+        map2.put("diligent","idel");
+        map1.put("guide","follow");
+        map1.put("flow","jam");
+
+
+        System.out.println(leftJoins(map1,map2));
+
     }
     // code challenge 31
     static String repeatedWord( String data){
@@ -44,6 +64,8 @@ public class Library {
         }
         return "no repeat word at the sentences";
     }
+
+    //     // code challenge 32
 
     static  ArrayList<String> intersection (BinarySearchTree Tree1, BinarySearchTree Tree2 ){
 
@@ -76,4 +98,29 @@ public class Library {
 
 
     }
+    // code challenge 33: Write a function that LEFT JOINs two hashmaps into a single data structure.
+
+    public static ArrayList<ArrayList<String>> leftJoins(HashMap<String,String> map1, HashMap<String,String> map2){
+        ArrayList<ArrayList<String>> bigContainer= new ArrayList<>();
+
+        ArrayList<String> smallContainer;
+
+        for (Map.Entry item: map1.entrySet()){
+            smallContainer= new ArrayList<>();
+            smallContainer.add((String) item.getKey());
+            smallContainer.add((String) item.getValue());
+
+            if (map2.containsKey(((String) item.getKey()))){
+                smallContainer.add(map2.get(item.getKey()));
+            }else{
+                smallContainer.add("Null");
+            }
+
+            bigContainer.add(smallContainer);
+        }
+
+        return bigContainer;
+    }
+
+
 }
